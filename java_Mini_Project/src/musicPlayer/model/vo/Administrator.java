b9022e17f22f089caa0bd6be829b9fcfdd33adfb
user2 <user2@user2-PC>
2019-05-03 16:59:10 +0900

    StoreMusic -> Administrator change

--- java_Mini_Project/src/musicPlayer/model/vo/Administrator.java
+++ java_Mini_Project/src/musicPlayer/model/vo/Administrator.java
@@ -10,12 +10,12 @@ import org.jaudiotagger.tag.FieldKey;
 import org.jaudiotagger.tag.Tag;
 import org.jaudiotagger.tag.id3.AbstractID3v2Tag;
 
-public class StoreMusic {
+public class Administrator {
 	
 	private List<Music> allMusic = new ArrayList<>();
 	long code = 0;
 	
-	public StoreMusic(String path) {
+	public Administrator(String path) {
 		File fs = new File(path);
 		if(fs.isDirectory()) {
 			File list[] = fs.listFiles();			
@@ -66,6 +66,49 @@ public class StoreMusic {
                 }
 			}
 		}
+	}
+	
+	public void addMusic() {
+		String path = "";
+		File f = new File(path);
+		
+		try{
+            MP3File mp3 = (MP3File) AudioFileIO.read(f);
+            AbstractID3v2Tag tag2 = mp3.getID3v2Tag();
+            
+            Tag tag = mp3.getTag();
+            String title = tag.getFirst(FieldKey.TITLE);
+            String artist = tag.getFirst(FieldKey.ARTIST);
+            String album = tag.getFirst(FieldKey.ALBUM);
+            String year = tag.getFirst(FieldKey.YEAR);
+            String genre = tag.getFirst(FieldKey.GENRE);
+//            Integer seconds = mp3.getAudioHeader().getTrackLength() + 1;//+1은 왜한거지?
+            Integer seconds = mp3.getAudioHeader().getTrackLength();
+            int minute = seconds/60;
+            int second = seconds%60;
+            
+     
+            //public Music(String path, long code, String title, String artist, String genre, String openYear, int like, int seconds);
+            Music music = new Music(f.toString(), code++, title, artist, genre, year, 0, seconds);
+            allMusic.add(music);
+            
+            System.out.println("Tag : " + tag2);
+            System.out.println("Song Name : " + title);
+            System.out.println("Artist : " + artist);
+            System.out.println("Album : " + album);
+            System.out.println("Year : " + year);
+            System.out.println("Genre : " + genre);
+            System.out.println("Seconds : "+seconds);
+            System.out.println("minute : "+minute); //4 ->실제 길이 4:23
+            System.out.println("second : "+second); //23 ->실제 길이 4:23
+            
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+	}
+	
+	public void removeMusic() {
+		
 	}
 	
 	public List<Music> getAllMusic(){
--- java_Mini_Project/src/musicPlayer/run/MusicPlayerRun.java
+++ java_Mini_Project/src/musicPlayer/run/MusicPlayerRun.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import musicPlayer.controller.PlayerController;
 import musicPlayer.model.vo.Music;
-import musicPlayer.model.vo.StoreMusic;
+import musicPlayer.model.vo.Administrator;
 import musicPlayer.view.PlayerView;
 
 public class MusicPlayerRun {
@@ -12,7 +12,7 @@ public class MusicPlayerRun {
 
 	public static void main(String[] args) {
 //		StoreMusic sm = new StoreMusic("C:\\Workspaces\\java_workspace\\java_Mini_Project\\musics");
-		StoreMusic sm = new StoreMusic("musics");
+		Administrator sm = new Administrator("musics");
 		allMusic = sm.getAllMusic();
 		new PlayerView(new PlayerController());
 	}
